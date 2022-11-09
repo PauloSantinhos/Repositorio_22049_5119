be31371d758f7c6e32a3d36754545507232eabb7
PauloSantinhos <1621864@formacao.iefp.pt>
2022-11-09 16:34:09 +0000

    Acrescentei após oi, Parece que funciona

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Oi");
+        System.out.println("Parece que funciona");
     }
 }
