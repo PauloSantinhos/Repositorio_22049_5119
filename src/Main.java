76e3d0d337c583ae08be45c71dcb1abb3ce6f0d5
PauloSantinhos <1621864@formacao.iefp.pt>
2022-11-09 16:36:51 +0000

    Acrescentei um comentário a especificar o que foi  feito

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+       // Acrescentei mais duas mensagens para testar commit
         System.out.println("Hello world!");
         System.out.println("Oi");
         System.out.println("Parece que funciona");
